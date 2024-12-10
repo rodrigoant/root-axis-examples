981aeb32abc691da4bab1a4f848fb394d8ba0bc5
Rodrigo Antunes <contatorodrigoantunes@gmail.com>
2024-12-10 11:09:16 -0300

    :bug: svelte config

--- packages/svelte/src/App.svelte
+++ packages/svelte/src/App.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import rootAxisLogo from '/root-axis.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import rootAxisLogo from "/root-axis.svg";
+
+  let count: number = $state(0);
+  const increment = () => {
+    count += 1;
+  };
 </script>
 
 <main>
@@ -13,24 +17,34 @@
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
-    <a href="https://design-system.rodrigoantunes.dev" target="_blank" rel="noreferrer">
+    <a
+      href="https://design-system.rodrigoantunes.dev"
+      target="_blank"
+      rel="noreferrer"
+    >
       <img src={rootAxisLogo} class="logo root axis" alt="Root Axis Logo" />
     </a>
-  <h1>Vite + Svelte</h1>
+    <h1>Vite + Svelte</h1>
   </div>
 
   <div class="card">
-    <Counter />
+    <ra-button onclick={increment}>
+      count is {count}
+    </ra-button>
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
   <div class="accordions">
     <ra-accordion dark>
       <span slot="summary">Is it accessible?</span>
-      Yes. 
+      Yes.
     </ra-accordion>
     <ra-accordion dark>
       <span slot="summary">Is it styled?</span>
@@ -41,7 +55,6 @@
       Yes. It&lsquo;s animated by default
     </ra-accordion>
   </div>
-
 </main>
 
 <style>
--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -4,4 +4,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      external: ['@root-axis/design-system']
+    }
+  },
+  optimizeDeps: {
+    include: ['@root-axis/design-system']
+  }
 })
