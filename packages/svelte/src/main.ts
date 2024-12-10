d63416c0aaff5a0ab8952608804e9891627ead89
Rodrigo Antunes <contatorodrigoantunes@gmail.com>
2024-12-10 13:21:59 -0300

    :bug: fix svelte import

--- packages/svelte/src/main.ts
+++ packages/svelte/src/main.ts
@@ -2,7 +2,7 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
-import '@root-axis/design-system';
+import '@root-axis/design-system/dist/ra-design-system.es';
 
 const app = mount(App, {
   target: document.getElementById('app')!,
